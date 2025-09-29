83fedc0d2e92061f67dd3dd9c2a34b5956710f35
GreenAlanMX <greenkeeper230@gmail.com>
2025-09-28 20:29:16 -0600

    página web

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // Configuración limpia sin PostCSS
 export default defineConfig({
   plugins: [react()],
+  base: '/My-page',
   // Removemos cualquier configuración de CSS/PostCSS
-})
+});
